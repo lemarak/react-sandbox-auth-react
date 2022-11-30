d84e1826e2a8c9549754ee9f23a63a89a357a849
Stéphane Dauguet <sdauguet@gmail.com>
2022-11-30 10:45:28 +0100

    logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
+import { signOut } from "firebase/auth";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/userContext";
+import { auth } from "../firebase-config";
 
 const Navbar = () => {
   const { toggleModals } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      alert(
+        "For some reasons, we can't deconnect. Please check your internet connexion and retry"
+      );
+    }
+  };
 
   return (
     <nav className="navbar navbar-ligth bg-light px-4">
@@ -23,7 +37,9 @@ const Navbar = () => {
         >
           Sign In
         </button>
-        <button className="btn btn-danger ms-3">Log Out</button>
+        <button onClick={logOut} className="btn btn-danger ms-3">
+          Log Out
+        </button>
       </div>
     </nav>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
+
 const Home = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <div className="container p-5">
-      <h1 className="display-3 text-light">Hi, sign up or sign in</h1>
+      <h1 className="display-3 text-light">
+        {currentUser
+          ? `Welcome ${currentUser.email}`
+          : "Hi, sign up or sign in"}
+      </h1>
     </div>
   );
 };
